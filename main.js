621412e2878f197176410cb00d18afb09870371f
BeenNjeri <119826204+BeenNjeri@users.noreply.github.com>
2023-04-18 05:21:40 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,68 @@
 // JavaScript source code
 let todo_wrapper = document.querySelector('.wrapper');
 let newTodo = document.querySelector('.todo_name');
+let newdd = document.querySelector('.todo_date');
 let addTodo = document.querySelector('.add_todo');
 let todo=[];
 
 addTodo.addEventListener('click',() => {
-    if(newTodo.nodeValue != ""){
-        todo.push(newTodo.value);
+    if(newTodo.value.length != 0){
+    //if(newTodo.nodeValue != ''){
+        todo.push(newTodo);
         console.log(todo)
 
     let newTodo_list = document.createElement('div');
+    var ddate = newdd.value;
+    var dddate = new Date(newdd.value);
+
         newTodo_list.className = 'item';
 
         for(let i = 0; i < todo.length; i++){
+//add info
             newTodo_list.innerHTML = newTodo.value;
+
+// add date
+            let duedate = document.createElement('Date');
+            duedate.className = 'duedate';
+            duedate.innerHTML= ddate.toLocaleString();
+            console.log(duedate);
+
+            //date alert
+            //now.setHours(0,0,0,0);
+            if (dddate < Date.now()){
+                duedate.style.color = "red";
+                console.log("old");
+            }else{
+                console.log("got time");
+            }
+
+
+/*priority (not finished)
+
+var level = document.querySelector('input[name="prior"]:checked').value;
+
+switch(true){
+    case level == null:
+
+       break;
+    case levl == 0:
+
+        break;
+    case level == 1:
+
+        break;
+
+    case level == 3:
+        break;
+
+    
+}
+*/
+
+            newTodo_list.appendChild(duedate);
             todo_wrapper.appendChild(newTodo_list);
         }
+//add delete
         if(todo.length > 0){
             let item=document.querySelectorAll('.item');
             for(let j = 0; j < item.length; j++){
@@ -29,6 +76,9 @@ addTodo.addEventListener('click',() => {
                 item[j].addEventListener("dragenter",dragEnter);                item[j].addEventListener("dragstart",dragStart);
                 item[j].addEventListener("dragdrop",drop);
                 item[j].addEventListener("dragleave",dragLeave);
+
+            
+
                 console.log(item[j]);
                 item[j].appendChild(delete_todo)
 
@@ -40,7 +90,7 @@ addTodo.addEventListener('click',() => {
 
                 }
             }            
-        newTodo.value = '';
+       newTodo.value = '';
     }
 })
 
